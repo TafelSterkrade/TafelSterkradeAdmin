9a671aa61a37890eb26be33f0d52ebe2b2ce3eb5
TafelSterkrade <tafel.sterkrade@web.de>
2025-10-28 08:49:18 +0100

    Update Apps_Script_URL.js with new date

--- Apps_Script_URL.js
+++ Apps_Script_URL.js
@@ -1,7 +1,8 @@
-// Apps_Script_URL.js (21.09.2025)
+// Apps_Script_URL.js (28.10.2025)
 
 //Testversion:
 //const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3FUjHbrBnoBDklKBr40VaM45-JOAdCHy8kVlvOx51kKeZnojumlTKisFFWewOZSR0Hg/exec";
 
 //Tafelversion:
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZguRQWHaQ6EsuQVu4H1fMYmHJXHf9eshUFn4eAIHJVm31ooCUAPTeUb92gtt_Iuzk/exec";
+
