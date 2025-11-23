0d9ae2929a740c2ab919260c9666cff93968c776
TafelSterkrade <tafel.sterkrade@web.de>
2025-11-23 23:27:40 +0100

    Delete Apps_Script_URL.js

--- Apps_Script_URL.js
+++
@@ -1,8 +0,0 @@
-// Apps_Script_URL.js (28.10.2025)
-
-//Testversion:
-//const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3FUjHbrBnoBDklKBr40VaM45-JOAdCHy8kVlvOx51kKeZnojumlTKisFFWewOZSR0Hg/exec";
-
-//Tafelversion:
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZguRQWHaQ6EsuQVu4H1fMYmHJXHf9eshUFn4eAIHJVm31ooCUAPTeUb92gtt_Iuzk/exec";
-
